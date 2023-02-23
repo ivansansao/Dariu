ba7dd364d7492f1ac25adef4e20b63c8dfa2b19c
ivansansao <aivanplus@gmail.com>
2023-02-23 02:15:18 -0300

    Space can Jump

--- src/model/dariu.cpp
+++ src/model/dariu.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include "dariu.hpp"
 #include "iostream"
 
@@ -6,24 +7,69 @@ Dariu::Dariu()
     dariu_tex.loadFromFile("./asset/dariu.png");
     dariu_spr.setTexture(dariu_tex);
     pos = sf::Vector2f(400.f, 400.f);
+    speed = sf::Vector2f(0.f, 0.f);
+    velocity = sf::Vector2f(0.f, 0.f);
+    ground_y = 680;
+    gravity = 0.9;
+    lift = -20;
 }
 void Dariu::update()
 {
-    float const speed = 4.f;
+
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
+    {
+        this->up();
+    }
+
+    velocity.y += gravity;
+    pos.y += velocity.y;
+    if (pos.y > ground_y)
+    {
+        pos.y = ground_y;
+        velocity.y = 0;
+    }
+
+    // Right left
 
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
     {
-        pos.x += speed;
+        velocity.x += 3;
+        velocity.x = std::min(5.f, velocity.x);
+    }
+    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+    {
+        velocity.x -= 3;
+        velocity.x = std::max(-5.f, velocity.x);
+    }
+    else
+    {
+        if (velocity.x > 0.f)
+        {
+            velocity.x -= 0.2f;
+            velocity.x = std::max(0.f, velocity.x);
+        }
+        else if (velocity.x < 0.f)
+        {
+            velocity.x += 0.2f;
+            velocity.x = std::min(0.f, velocity.x);
+        }
     }
 
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-    {
-        pos.x -= speed;
-    }
+    pos.x += velocity.x;
 
     dariu_spr.setPosition(pos);
 }
 void Dariu::draw(sf::RenderWindow *w)
 {
     w->draw(dariu_spr);
+}
+bool Dariu::onFloor()
+{
+    return (pos.y == ground_y);
+}
+
+void Dariu::up()
+{
+    if (this->onFloor())
+        velocity.y += lift;
 }
--- src/model/dariu.hpp
+++ src/model/dariu.hpp
@@ -1,6 +1,5 @@
 #ifndef DARIU_H
 #define DARIU_H
-
 #include <SFML/Graphics.hpp>
 
 class Dariu
@@ -14,8 +13,15 @@ public:
     sf::Sprite dariu_spr;
 
     sf::Vector2f pos;
+    sf::Vector2f speed;
+    sf::Vector2f velocity;
+    float ground_y;
+    float gravity;
+    float lift;
 
     void update();
     void draw(sf::RenderWindow *w);
+    void up();
+    bool onFloor();
 };
 #endif
