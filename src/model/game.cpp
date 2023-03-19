d705b4b1ad8131fd4ccab5784066ab0316730d64
Ivan Sansão <aivanplus@gmail.com>
2023-03-19 14:36:18 -0300

    Removed version from title

--- src/model/game.cpp
+++ src/model/game.cpp
@@ -11,8 +11,8 @@
 using namespace std;
 
 Game::Game() {
-    window.create(sf::VideoMode(sf::VideoMode::getDesktopMode().width, sf::VideoMode::getDesktopMode().height, 32), "Dariu - 0.6", sf::Style::Fullscreen);
-    // window.create(sf::VideoMode(1280, 736), "Dariu - 0.6", sf::Style::Titlebar | sf::Style::Close);
+    window.create(sf::VideoMode(sf::VideoMode::getDesktopMode().width, sf::VideoMode::getDesktopMode().height, 32), "Dariu", sf::Style::Fullscreen);
+    // window.create(sf::VideoMode(1280, 736), "Dariu", sf::Style::Titlebar | sf::Style::Close);
     window.setFramerateLimit(60);
     window.setPosition(sf::Vector2i(0, 0));
     window.setMouseCursorVisible(false);
