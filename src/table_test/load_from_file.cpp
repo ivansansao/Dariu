ebfc917473791b106e1da5a0a211e03c732b4dbb
ivansansao <aivanplus@gmail.com>
2023-05-08 21:13:44 -0300

    new load_from_file table test

--- src/table_test/load_from_file.cpp
+++ src/table_test/load_from_file.cpp
@@ -2,10 +2,25 @@
 // a vector of class objects
 #include <fstream>
 #include <iostream>
+#include <regex>
 #include <string>
 
-using namespace std;
-
+int wordOccurrence(std::string sentence, std::string word) {
+    int slen = sentence.length();
+    int wordlen = word.length();
+    int count = 0;
+    int i, j;
+    for (i = 0; i < slen; i++) {
+        for (j = 0; j < wordlen; j++) {
+            if (sentence[i + j] != word[j])
+                break;
+        }
+        if (j == wordlen) {
+            count++;
+        }
+    }
+    return count;
+}
 bool hasString(std::string line, std::string str) {
     int i_string = (int)line.find(str);
     if (i_string > 999)
@@ -16,9 +31,9 @@ bool hasString(std::string line, std::string str) {
 std::string get_lines_from_file(std::string filename, std::string tag, std::string endLine) {
     bool recording = tag.empty() || tag == "[]";
     std::string chunk = "";
-    string line;
+    std::string line;
 
-    ifstream map_file(filename);
+    std::ifstream map_file(filename);
 
     while (getline(map_file, line)) {
         if (recording && hasString(line, "[ENDBLOCK]")) {
@@ -38,7 +53,15 @@ std::string get_lines_from_file(std::string filename, std::string tag, std::stri
 }
 
 void load_from_file(int phase) {
-    cout << get_lines_from_file("../resource/map_" + to_string(phase) + ".dtm", "[THEME]", "") << "FIM";
+    std::string lines = get_lines_from_file("../resource/map_" + std::to_string(phase) + ".dtm", "", "\n");
+    std::stringstream ss(lines);
+    std::string line;
+    int q_lines = wordOccurrence(lines, "\n");
+
+    while (getline(ss, line, '\n')) {
+        std::cout << line << "\n";
+    }
+    std::cout << "Linhas: " << q_lines << std::endl;
 }
 
 int main() {
