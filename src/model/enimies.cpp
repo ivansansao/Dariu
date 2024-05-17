9c1cbf7517dc85e94cd1633eaf8ac4a0bdcd9ccd
ivansansao <aivanplus@gmail.com>
2024-05-17 20:20:19 -0300

    Change Catraca character, change phases

--- src/model/enimies.cpp
+++ src/model/enimies.cpp
@@ -174,7 +174,7 @@ void Enimy::die(Tilemap *tilemap, Sounds *sounds) {
 
 Catraca::Catraca() {
     actor_tex_fall.loadFromFile("./src/asset/image/Main Characters/Mask Dude/Fall (32x32).png");
-    actor_tex.loadFromFile("./src/asset/image/Main Characters/Mask Dude/Run (32x32).png");
+    actor_tex.loadFromFile("./src/asset/image/catraca.png");
     actor_tex_idle.loadFromFile("./src/asset/image/Main Characters/Mask Dude/Idle (32x32).png");
 };
 
@@ -182,7 +182,17 @@ void Catraca::update(Tilemap *tilemap, Sounds *sounds) {
     Enimy::update(tilemap, sounds);
 }
 void Catraca::draw(sf::RenderWindow *w) {
-    Enimy::draw(w);
+    actor_spr.setPosition(pos.left, pos.top);
+
+    if (velocity.x == 0) {
+        actor_spr.setTexture(actor_tex_idle);
+        actor_spr.setTextureRect(sf::IntRect(Tools::getStartSprite((int)i_idle_sprite % 7, direction_x) * 32, 0, direction_x * 32, 32));
+        i_idle_sprite += 0.2;
+    } else {
+        actor_spr.setTexture(actor_tex);
+        actor_spr.setTextureRect(sf::IntRect(Tools::getStartSprite((int)pos.left % 8, direction_x) * 32, 0, direction_x * 32, 32));
+    }
+    w->draw(actor_spr);
 }
 void Catraca::on_collide(std::string where, int i, int j, Tilemap *tilemap, Sounds *sounds) {
     Enimy::on_collide(where, i, j, tilemap, sounds);
