34a3c3aeee3b47aa6bfab1ed8e2cbf3b0b15c99b
Ivan Sansão <aivanplus@gmail.com>
2026-01-16 21:44:15 -0300

    Improve title string

--- src/model/dariu.cpp
+++ src/model/dariu.cpp
@@ -106,7 +106,8 @@ void Dariu::draw(sf::RenderWindow* w, int phase, int phase_total, int curr_milis
     xscore += L"          Artefatos: ";
     xscore += to_string(score.thophy) + "/" + to_string(score.thophy_total);
     xscore += "          Fase: " + to_string(phase) + " / " + to_string(phase_total);
-    xscore += "          Liquor: " + to_string(score.liquor);
+    xscore += L"          Cachaça: ";
+    xscore += to_string(score.liquor);
 
     if (this->jetPackFuel > 0) {
         xscore += "          Mochila a jato: " + to_string((int)this->jetPackFuel) + " / 100";
